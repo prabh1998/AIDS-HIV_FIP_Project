1f183d5764976aef65fec951834d571ee04d8a3f
Your Name <you@example.com>
2020-03-05 18:02:28 -0500

    user page created

--- load.php
+++ load.php
@@ -9,6 +9,7 @@ require_once ABSPATH.'/config/database.php';
 require_once ADMIN_SCRIPT_PATH.'/functions.php';
 require_once ADMIN_SCRIPT_PATH.'/sign_up.php';
 require_once ADMIN_SCRIPT_PATH.'/login.php';
+require_once ADMIN_SCRIPT_PATH.'/user.php';
 require_once 'phpmailer/PHPMailer.php';
 require_once 'phpmailer/Exception.php';
 require_once 'phpmailer/SMTP.php';
